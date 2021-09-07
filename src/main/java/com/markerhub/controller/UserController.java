de9dad21ee11afb57e911c444a09646ce02e406b
vanplanet <916505389@qq.com>
2021-09-08 00:53:43 +0800

    提交

---
+++ src/main/java/com/markerhub/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.markerhub.controller;
+
+
+import com.markerhub.common.lang.Result;
+import com.markerhub.entity.User;
+import com.markerhub.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 关注公众号：MarkerHub
+ * @since 2020-05-25
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+
+    @PostMapping("/save")
+    public Result save(@Validated @RequestBody User user) {
+
+        return Result.succ(user
+
+        );
+    }
+
+}
